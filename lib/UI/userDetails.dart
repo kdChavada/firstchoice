5e31bb07fe95ca37944928d19c192ba64012b098
ABCD <kkweb99@gmail.com>
2021-02-04 18:38:28 +0530

    Add Model And Show data Form Api user List Show 04-02-2021

--- lib/UI/userDetails.dart
+++ lib/UI/userDetails.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:firstchoice/repository/senddata.dart';
-import 'package:http/http.dart' as http;
-import 'package:flutter/material.dart';
 
+import 'package:firstchoice/repository/senddata.dart';
+import 'package:firstchoice/repository/userlist.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class UserDetails extends StatefulWidget {
   @override
@@ -11,41 +13,37 @@ class UserDetails extends StatefulWidget {
 }
 
 class _UserDetailsState extends State<UserDetails> {
+  Future<UserList> futureData;
+  var datalength;
 
-
-
-
-
-
-
-
-
-
-  void getUserDetails() async {
-
-      http.Response response = await http.get("http://firstchoice.9brainz.store/api/v1/users",headers: {
-        HttpHeaders.contentTypeHeader: "application/json", HttpHeaders.authorizationHeader: "Bearer $accessToken"
+  Future<UserList> getUserDetails() async {
+    http.Response response = await http.get(
+      "http://firstchoice.9brainz.store/api/v1/users",
+      headers: {
+        HttpHeaders.contentTypeHeader: "application/json",
+        HttpHeaders.authorizationHeader: "Bearer $accessToken"
       },
+    );
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      datalength = data['user'].length;
+      print(data['user']);
+      return UserList.formJson(data['user'][1]);
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
 
-      );
-      if (response.statusCode == 200) {
-
-        var dataget = jsonDecode(response.body);
-        print(dataget['user']);
-
-      } else {
-        // If the server did not return a 201 CREATED response,
-        // then throw an exception.
-        throw Exception('Failed to load data');
-      }
-
-
+  @override
+  void initState() {
+    futureData = getUserDetails();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    double  h  = MediaQuery.of(context).size.height;
-    double  w  = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: AppBar(
@@ -53,19 +51,111 @@ class _UserDetailsState extends State<UserDetails> {
         centerTitle: true,
         title: Text("User List"),
       ),
-      body: Center(
-        child: GestureDetector(
-          onTap: (){
-            getUserDetails();
-            print(accessToken);
-          },
-          child: Container(
-            height: h*0.08,
-            width: w*0.3,
-            color: Colors.blue,
-          ),
-        ),
+      body: ListView.builder(
+        itemCount: datalength == null ? 0 : datalength,
+        itemBuilder: (context, index) {
+          return Container(
+            child: Center(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // Card(
+                //   child: Container(
+                //     child: Text(datalength.toString()),
+                //     padding: EdgeInsets.all(20.0),
+                //   ),
+                // ),
+
+                Card(
+                  child: Container(
+                    child: FutureBuilder<UserList>(
+                      future: futureData,
+                      builder: (context, val) {
+                        if (val.hasData) {
+                          return Container(
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  Text("Name"),
+                                  Text("${val.data.name}"),
+                                  Text("Phone No"),
+                                  Text(" ${val.data.phoneNo}"),
+                                  Text("Email"),
+                                  Text(" ${val.data.email}"),
+                                  Text("Adress"),
+                                  Text(" ${val.data.address}"),
+                                  Text("Gender"),
+                                  Text(" ${val.data.gender}"),
+                                  Text("Country"),
+                                  Text(" ${val.data.country}"),
+                                  Text("Height"),
+                                  Text(" ${val.data.height}"),
+                                  Text("BirthPlace"),
+                                  Text("${val.data.birth_place}"),
+                                  Text("Birthdate"),
+                                  Text("${val.data.birth_date}"),
+                                  Text("BirthTime"),
+                                  Text("${val.data.birth_time}"),
+                                  Text("livedin"),
+                                  Text("${val.data.lived_in}"),
+                                  Text("about User"),
+                                  Text("${val.data.about_user}"),
+                                  Text("material status"),
+                                  Text("${val.data.material_status}"),
+                                  Text("occuption"),
+                                  Text("${val.data.occupation}"),
+                                  Text("interest"),
+                                  Text("${val.data.interest}"),
+                                  Text("created at"),
+                                  Text("${val.data.created_at}"),
+                                  Text("updated at"),
+                                  Text("${val.data.updated_at}"),
+                                ],
+                              ),
+                            ),
+                          );
+                        } else if (val.hasError) {
+                          return Text("${val.error}");
+                        }
+                        return CircularProgressIndicator();
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            )),
+          );
+        },
       ),
+      // body: FutureBuilder<UserList>(
+      //   future: futureData,
+      //   builder: (context, val) {
+      //     if (val.hasData) {
+      //       return Container(
+      //         child: SingleChildScrollView(
+      //           child: Column(
+      //             children: [
+      //
+      //
+      //               Text("Name${val.data.name}"),
+      //               Text("PhoneNo${val.data.phoneNo}"),
+      //               Text("Email${val.data.email}"),
+      //               Text("Adress${val.data.address}"),
+      //               Text("Gender${val.data.gender}"),
+      //               Text("Country${val.data.country}"),
+      //               Text("height${val.data.height}"),
+      //
+      //
+      //             ],
+      //           ),
+      //         ),
+      //       );
+      //     } else if (val.hasError) {
+      //       return Text("${val.error}");
+      //     }
+      //     return CircularProgressIndicator();
+      //   },
+      // ),
     );
   }
 }
--- lib/repository/userlist.dart
+++ lib/repository/userlist.dart
@@ -1,6 +1,69 @@
-class  Userlist  {
+import 'package:flutter/cupertino.dart';
 
+class UserList {
+  int id;
   String name;
+  String phoneNo;
+  String email;
+  String address;
+  String gender;
+  String country;
+  String height;
+  String birth_place;
+  String birth_date;
+  String birth_time;
+  String lived_in;
+  String about_user;
+  String material_status;
+  String occupation;
+  String interest;
+  String created_at;
+  String updated_at;
 
 
+  UserList(
+      {
+        @required this.id,
+          this.name,
+       this.phoneNo,
+      this.email,
+      this.address,
+      this.gender,
+      this.country,
+      this.height,
+      this.birth_place,
+        this.birth_date,
+        this.birth_time,
+        this.lived_in,
+        this.about_user,
+        this.material_status,
+        this.occupation,
+        this.interest,
+        this.created_at,
+        this.updated_at,
+
+      });
+
+  factory UserList.formJson(Map<String, dynamic> json) {
+    return UserList(
+        id: json['id'],
+        name: json['name'],
+        phoneNo: json['phone_no'],
+        email: json['email'],
+        address: json['address'],
+        gender: json['gender'],
+        country: json['country'],
+        birth_place: json['birth_place'],
+        birth_date: json['birth_date'],
+        birth_time: json['birth_time'],
+        lived_in: json['lived_in'],
+        about_user: json['about_user'],
+        material_status:  json['material_status'],
+        occupation: json['occupation'],
+        interest: json['interest'],
+        created_at: json['created_at'],
+        updated_at: json['updated_at'],
+        height: json['height'].toString());
+
+  }
 }
